33d7d56fd3d7819b12aea4ff319e0b382a7f8dbf
Rolf B <rolf.bjarenstam@gmail.com>
2018-07-10 11:41:02 +0200

    Add project files.

---
+++ GymApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymApp.Startup))]
+namespace GymApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
