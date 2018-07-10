8794c7db71784bcad7f671af8dd83b606da84461
Rolf B <rolf.bjarenstam@gmail.com>
2018-07-10 16:48:55 +0200

    Version 0.2

--- GymApp/Controllers/GymClassesController.cs
+++ GymApp/Controllers/GymClassesController.cs
@@ -10,6 +10,25 @@ namespace GymApp.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
+        public ActionResult BookingToggle(int id)
+        {
+            GymClass CurrentClass = db.GymClasses.Where(g => g.Id == id).FirstOrDefault();
+            ApplicationUser CurrentUser = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+
+            if (CurrentClass.AttendingMembers.Contains(CurrentUser))
+            {
+                CurrentClass.AttendingMembers.Remove(CurrentUser);
+                db.SaveChanges();
+            }
+            else
+            {
+                CurrentClass.AttendingMembers.Add(CurrentUser);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
         // GET: GymClasses
         public ActionResult Index()
         {
