cad94b04d6143824d86ce75cf86a44b3c7c5a553
Rolf B <rolf.bjarenstam@gmail.com>
2018-07-10 14:57:17 +0200

    Initial revision of GymApp, inlcuding initial database.

---
+++ GymApp/Models/GymClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GymApp.Models
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        public string Description { get; set; }
+
+        // Navigational property
+        public virtual ICollection<ApplicationUser> AttendingMembers { get; set; }
+    }
+}
